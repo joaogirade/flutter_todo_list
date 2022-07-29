848f85541c9ee030b597e64697584c668d9c579d
João Girade <joao.girade@fiorilli.com.br>
2022-07-29 14:01:30 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/todo_list_page.dart';
+
+void main() {
+  runApp(AppToDo());
+}
+
+class AppToDo extends StatelessWidget {
+  const AppToDo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TodoListPage(),
+    );
+  }
+}
+
---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  const TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicione uma Tarefa',
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.teal,
+                ),
+                child: Icon(
+                  Icons.add,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
