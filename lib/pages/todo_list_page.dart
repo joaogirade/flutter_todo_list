8bd9c7fd10f762d696f7903470600d355ccbac25
João Girade <joao.girade@fiorilli.com.br>
2022-07-29 15:43:57 -0300

    "Clear All" button and Task Status

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -9,30 +9,58 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicione uma Tarefa',
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma Tarefa',
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff084d6e),
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
               ),
               SizedBox(
-                width: 8,
+                height: 16,
               ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xff084d6e),
-                  padding: EdgeInsets.all(14),
-                ),
-                child: Icon(
-                  Icons.add,
-                  size: 30,
-                ),
-              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Você possui 0 tarefas pendentes.',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xff084d6e),
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Text('Limpar Tudo'),
+                  )
+                ],
+              )
             ],
           ),
         ),
